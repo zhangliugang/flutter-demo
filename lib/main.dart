4093993f1f4fc281d8452a4f7c8688f5d06e0f91
zhangliugang <zhangliugang@gmail.com>
2019-01-04 18:00:17 +0800

    4

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,20 @@ class MainPage extends StatelessWidget {
           onTap: () {
             Navigator.push(context, MaterialPageRoute(builder: (_) => ImagePage()));
           },
+        ),
+        Container(
+          height: 200.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(width: 160, color: Colors.red),
+              Container(width: 160, color: Colors.blue),
+              Container(width: 160, color: Colors.green),
+              Container(width: 160, color: Colors.purple),
+              Container(width: 160, color: Colors.yellow),
+              Container(width: 160, color: Colors.orange),
+            ],
+          ),
         )
       ],
     );
